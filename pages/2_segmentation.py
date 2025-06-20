b63c335ee10bec05df723df09c632ae90f69bf25
Christophe BAJAN <108456770+ChristopheBaj@users.noreply.github.com>
2025-06-20 16:37:42 +0900

    Update 2_segmentation.py

--- pages/2_segmentation.py
+++ pages/2_segmentation.py
@@ -54,7 +54,8 @@ if uploaded_file:
         label="Download Segmentation",
         data=best_mask.astype(np.uint8)*255,
         file_name="segmentation_.png",
-        mime="png"
+        mime="png",
+        icon=":material/download:",
     )
 
     # --- Region Measurements ---
